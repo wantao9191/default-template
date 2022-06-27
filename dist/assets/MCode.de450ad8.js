268d6c81d0ca7a5b363e46bf1b851712f2dc06a0
467112199@qq.com <467112199@qq.com>
2022-06-27 22:11:35 +0800

    igone

--- dist/assets/MCode.de450ad8.js
+++
@@ -1,1 +0,0 @@
-import{l as s}from"./index.204c51f9.js";const r={props:{value:{type:String,default:""}},setup(e,p){return()=>e.value.split(",").map(t=>(t=t.split("&"),s("span",{class:`code-tag ${t.length>1?"code-script-"+t[0]:""}`},t.length>1?t[1]:t[0])))}};export{r as _};
